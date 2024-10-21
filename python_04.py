863927964372b1b1ad1b3f21173fd570efa2cd12
kadir-zeren <k4dzeren@gmail.com>
2024-10-21 17:46:59 +0300

    first commit

---
+++ python_04.py
@@ -0,0 +1,6 @@
+fruit = 'Orange'
+print( 'Word :', fruit)
+print('First letter:', fruit[0])
+print('Second letter:', fruit[1])
+print("3rd to 5th letters:", fruit[2:5])
+print("Letter all after 3rd:" , fruit[2:])
